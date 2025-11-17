a0ed167b044ef6adfa4b5709fe35942b3042ad2f
Dinusha Fernando <dinushafdo9998@gmail.com>
2025-11-17 10:24:48 +0530

    Backend update

--- AETI_backend/contacts/views.py
+++ AETI_backend/contacts/views.py
@@ -2,9 +2,6 @@ from rest_framework import viewsets
 from .models import ContactDetails
 from .serializers import ContactDetailsSerializer
 
-class ContactDetailsViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    Read-only endpoint for site contact details
-    """
+class ContactDetailsViewSet(viewsets.ModelViewSet):
     queryset = ContactDetails.objects.all()
     serializer_class = ContactDetailsSerializer
--- AETI_backend/staff/urls.py
+++ AETI_backend/staff/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import DownloadViewSet
+from .views import StaffViewSet
 
 router = DefaultRouter()
-router.register(r'', DownloadViewSet, basename='download')
+router.register(r'', StaffViewSet, basename='staff')
 
 urlpatterns = router.urls
